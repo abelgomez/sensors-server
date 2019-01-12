82051fea4d71a7ecdd1c38755ff39a5e765cb2da
Abel Gómez <abgolla@gmail.com>
2019-01-12 19:13:33 +0100

    Small refactoring

--- rest-server.py
+++ rest-server.py
@@ -13,9 +13,10 @@ GPIO_SENSOR = 23
 INTERVAL = 10
 LISTEN=8080
 
-class Dht22:
+class DhtXX:
 
     def __init__(self):
+        self.type = Adafruit_DHT.DHT22
         self.refresh_interval = INTERVAL
         self.last_update = None
         self.temperature = None
@@ -28,7 +29,7 @@ class Dht22:
 
     def __update(self):
         self.last_update = str(datetime.datetime.now())
-        (h, t) = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, GPIO_SENSOR)
+        (h, t) = Adafruit_DHT.read_retry(self.type, GPIO_SENSOR)
         if t is not None:
             self.temperature = round(t,1)
         if h is not None:
@@ -39,23 +40,23 @@ class Dht22:
             data.__update()
             time.sleep(self.refresh_interval)
 
-class dht22(tornado.web.RequestHandler):
+class dhtxx(tornado.web.RequestHandler):
     def get(self):
         format = self.get_argument('output', 'json')
         if format == 'json':
-            self.write(dht22.__dict__)
+            self.write(dhtxx.__dict__)
         elif format == 'raw-temperature':
-            self.write("{0:.1f}".format(dht22.temperature))
+            self.write("{0:.1f}".format(dhtxx.temperature))
         elif format == 'raw-humidity':
-            self.write("{0:.1f}".format(dht22.humidity))
+            self.write("{0:.1f}".format(dhtxx.humidity))
         else:
             self.write('Invalid argument')
 
 application = tornado.web.Application([
-        (r"/api/dht22", dht22),
+        (r"/api/dht", dhtxx),
 ])
 
-dht22 = Dht22()
+dhtxx = DhtXX()
 
 if __name__ == "__main__":
     application.listen(LISTEN, "0.0.0.0")
