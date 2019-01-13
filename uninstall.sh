3e9eb4d3e21583d7db182ae7bd8aa490b599cd2a
Abel Gómez <abgolla@gmail.com>
2019-01-13 03:40:46 +0100

    Update uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -1,14 +1,19 @@
 #! /usr/bin/env bash
 
-INSTDIR=/opt/sensors
-SVCS_LOCATION=/etc/systemd/system/
+INSTDIR="/opt/sensors"
+SVC_LOCATION="/etc/systemd/system"
+SVC_FILE="$SVC_LOCATION/sensors-server.service"
+CONF_LOCATION="$SVC_FILE.d"
+CONF_FILE="$CONF_LOCATION/override.conf"
 
 [ "$(whoami)" != "root" ] && exec sudo -p "[sudo] This script must be executed as root. Password for %p: " -- "$0" "$@"
 
 systemctl stop sensors-server
 systemctl disable sensors-server
-rm $SVCS_LOCATION/sensors-server.service
+
+rm $SVC_FILE
+rm $CONF_FILE
 rm $INSTDIR/rest-server.py
 rmdir $INSTDIR
-
-
+rmdir $SVC_LOCATION
+rmdir $CONF_LOCATION
